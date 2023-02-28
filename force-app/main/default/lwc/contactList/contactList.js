5caf1448f4f4351e5714721bf07555d23c258e94
Edwin Estrada <edwin.estro@me.com>
2023-02-28 01:30:02 -0600

    LWC + Data

--- force-app/main/default/lwc/contactList/contactList.js
+++ force-app/main/default/lwc/contactList/contactList.js
@@ -1,14 +1,26 @@
 import { LightningElement, wire } from 'lwc';
+import FIRSTNAME_FIELD from '@salesforce/schema/Contact.FirstName';
+import LASTNAME_FIELD from '@salesforce/schema/Contact.LastName';
+import EMAIL_FIELD from '@salesforce/schema/Contact.Email';
 import getContacts from '@salesforce/apex/ContactController.getContacts';
-import {reduceErrors} from 'c/ldsUtils';
-export default class ContactList extends LightningElement {
-    @wire(getContacts) contacts;
+import { reduceErrors } from 'c/ldsUtils';
 
+
+
+
+const COLUMNS = [
+    { label: 'First Name', fieldName: FIRSTNAME_FIELD.fieldApiName, type: 'text' },
+    { label: 'Last Name', fieldName: LASTNAME_FIELD.fieldApiName, type: 'text' },
+    { label: 'Email', fieldName: EMAIL_FIELD.fieldApiName, type: 'Email' }
+];
+
+export default class MyComponent extends LightningElement {
+    columns = COLUMNS;
+    @wire(getContacts)
+    contacts;
     get errors() {
         return (this.contacts.error) ?
             reduceErrors(this.contacts.error) : [];
     }
-
-
 
 }
